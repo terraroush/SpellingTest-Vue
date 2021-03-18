036517d736ff65458805c9fd5bda89db0fdbe903
Terra Roush <terra.roush@gmail.com>
2021-03-18 13:48:34 -0500

    perfect score conditional

---
+++ src/components/Score.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <p>Results: {{ correctCount }} / {{ data.length }}</p>
+    <p v-if="perfectScore">PERFECT SCORE!!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["data"],
+
+  computed: {
+    correctCount() {
+      return this.data.filter((q) => {
+        return q.word.toLowerCase() === q.userInput.toLowerCase();
+      }).length;
+    },
+    perfectScore() {
+        return this.correctCount === this.data.length;
+    }
+  },
+};
+</script>
+
+<style></style>
