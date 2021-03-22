b026a6b08ea1121ee5028d421f81150e99f0a4fd
Terra Roush <terra.roush@gmail.com>
2021-03-22 08:16:29 -0500

    deployed on firebase

--- src/data.js
+++ src/data.js
@@ -7,134 +7,134 @@ export default [
     word: "gonna",
     userInput: null,
   },
-//   {
-//     word: "give",
-//     userInput: null,
-//   },
-//   {
-//     word: "you",
-//     userInput: null,
-//   },
-//   {
-//     word: "up",
-//     userInput: null,
-//   },
-//   {
-//     word: "never",
-//     userInput: null,
-//   },
-//   {
-//     word: "gonna",
-//     userInput: null,
-//   },
-//   {
-//     word: "let",
-//     userInput: null,
-//   },
-//   {
-//     word: "you",
-//     userInput: null,
-//   },
-//   {
-//     word: "down",
-//     userInput: null,
-//   },
-//   {
-//     word: "never",
-//     userInput: null,
-//   },
-//   {
-//     word: "gonna",
-//     userInput: null,
-//   },
-  //   {
-  //     word: "run",
-  //     userInput: null,
-  //   },
-  //   {
-  //     word: "around",
-  //     userInput: null,
-  //   },
-  //   {
-  //     word: "and",
-  //     userInput: null,
-  //   },
-  //   {
-  //     word: "dessert",
-  //     userInput: null,
-  //   },
-  //   {
-  //     word: "you",
-  //     userInput: null,
-  //   },
-  //   {
-  //     word: "never",
-  //     userInput: null,
-  //   },
-  //   {
-  //     word: "gonna",
-  //     userInput: null,
-  //   },
-  //   {
-  //     word: "make",
-  //     userInput: null,
-  //   },
-  //   {
-  //     word: "you",
-  //     userInput: null,
-  //   },
-  //   {
-  //     word: "cry",
-  //     userInput: null,
-  //   },
-  //   {
-  //     word: "never",
-  //     userInput: null,
-  //   },
-  //   {
-  //     word: "gonna",
-  //     userInput: null,
-  //   },
-  //   {
-  //     word: "say",
-  //     userInput: null,
-  //   },
-  //   {
-  //     word: "goodbye",
-  //     userInput: null,
-  //   },
-  //   {
-  //     word: "never",
-  //     userInput: null,
-  //   },
-  //   {
-  //     word: "gonna",
-  //     userInput: null,
-  //   },
-  //   {
-  //     word: "tell",
-  //     userInput: null,
-  //   },
-  //   {
-  //     word: "a",
-  //     userInput: null,
-  //   },
-  //   {
-  //     word: "lie",
-  //     userInput: null,
-  //   },
-  //   {
-  //     word: "and",
-  //     userInput: null,
-  //   },
-  //   {
-  //     word: "hurt",
-  //     userInput: null,
-  //   },
-  //   {
-  //     word: "you",
-  //     userInput: null,
-  //   },
+  {
+    word: "give",
+    userInput: null,
+  },
+  {
+    word: "you",
+    userInput: null,
+  },
+  {
+    word: "up",
+    userInput: null,
+  },
+  {
+    word: "never",
+    userInput: null,
+  },
+  {
+    word: "gonna",
+    userInput: null,
+  },
+  {
+    word: "let",
+    userInput: null,
+  },
+  {
+    word: "you",
+    userInput: null,
+  },
+  {
+    word: "down",
+    userInput: null,
+  },
+  {
+    word: "never",
+    userInput: null,
+  },
+  {
+    word: "gonna",
+    userInput: null,
+  },
+  {
+    word: "run",
+    userInput: null,
+  },
+  {
+    word: "around",
+    userInput: null,
+  },
+  {
+    word: "and",
+    userInput: null,
+  },
+  {
+    word: "dessert",
+    userInput: null,
+  },
+  {
+    word: "you",
+    userInput: null,
+  },
+  {
+    word: "never",
+    userInput: null,
+  },
+  {
+    word: "gonna",
+    userInput: null,
+  },
+  {
+    word: "make",
+    userInput: null,
+  },
+  {
+    word: "you",
+    userInput: null,
+  },
+  {
+    word: "cry",
+    userInput: null,
+  },
+  {
+    word: "never",
+    userInput: null,
+  },
+  {
+    word: "gonna",
+    userInput: null,
+  },
+  {
+    word: "say",
+    userInput: null,
+  },
+  {
+    word: "goodbye",
+    userInput: null,
+  },
+  {
+    word: "never",
+    userInput: null,
+  },
+  {
+    word: "gonna",
+    userInput: null,
+  },
+  {
+    word: "tell",
+    userInput: null,
+  },
+  {
+    word: "a",
+    userInput: null,
+  },
+  {
+    word: "lie",
+    userInput: null,
+  },
+  {
+    word: "and",
+    userInput: null,
+  },
+  {
+    word: "hurt",
+    userInput: null,
+  },
+  {
+    word: "you",
+    userInput: null,
+  },
 ];
 
 // export default [
