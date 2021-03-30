63bff785c0697d0c0cf0e8b0514ebf9420ec7152
Terra Roush <terra.roush@gmail.com>
2021-03-30 16:58:48 -0500

    changed word bank to be less annoying

--- src/data.js
+++ src/data.js
@@ -1,213 +1,213 @@
+// export default [
+//   {
+//     word: "never",
+//     userInput: null,
+//   },
+//   {
+//     word: "gonna",
+//     userInput: null,
+//   },
+//   {
+//     word: "give",
+//     userInput: null,
+//   },
+//   {
+//     word: "you",
+//     userInput: null,
+//   },
+//   {
+//     word: "up",
+//     userInput: null,
+//   },
+//   {
+//     word: "never",
+//     userInput: null,
+//   },
+//   {
+//     word: "gonna",
+//     userInput: null,
+//   },
+//   {
+//     word: "let",
+//     userInput: null,
+//   },
+//   {
+//     word: "you",
+//     userInput: null,
+//   },
+//   {
+//     word: "down",
+//     userInput: null,
+//   },
+//   {
+//     word: "never",
+//     userInput: null,
+//   },
+//   {
+//     word: "gonna",
+//     userInput: null,
+//   },
+//   {
+//     word: "run",
+//     userInput: null,
+//   },
+//   {
+//     word: "around",
+//     userInput: null,
+//   },
+//   {
+//     word: "and",
+//     userInput: null,
+//   },
+//   {
+//     word: "dessert",
+//     userInput: null,
+//   },
+//   {
+//     word: "you",
+//     userInput: null,
+//   },
+//   {
+//     word: "never",
+//     userInput: null,
+//   },
+//   {
+//     word: "gonna",
+//     userInput: null,
+//   },
+//   {
+//     word: "make",
+//     userInput: null,
+//   },
+//   {
+//     word: "you",
+//     userInput: null,
+//   },
+//   {
+//     word: "cry",
+//     userInput: null,
+//   },
+//   {
+//     word: "never",
+//     userInput: null,
+//   },
+//   {
+//     word: "gonna",
+//     userInput: null,
+//   },
+//   {
+//     word: "say",
+//     userInput: null,
+//   },
+//   {
+//     word: "goodbye",
+//     userInput: null,
+//   },
+//   {
+//     word: "never",
+//     userInput: null,
+//   },
+//   {
+//     word: "gonna",
+//     userInput: null,
+//   },
+//   {
+//     word: "tell",
+//     userInput: null,
+//   },
+//   {
+//     word: "a",
+//     userInput: null,
+//   },
+//   {
+//     word: "lie",
+//     userInput: null,
+//   },
+//   {
+//     word: "and",
+//     userInput: null,
+//   },
+//   {
+//     word: "hurt",
+//     userInput: null,
+//   },
+//   {
+//     word: "you",
+//     userInput: null,
+//   },
+// ];
+
 export default [
   {
-    word: "never",
+    word: "nauseous",
     userInput: null,
   },
   {
-    word: "gonna",
+    word: "dilate",
     userInput: null,
   },
   {
-    word: "give",
+    word: "indict",
     userInput: null,
   },
   {
-    word: "you",
+    word: "liquefy",
     userInput: null,
   },
   {
-    word: "up",
+    word: "bologna",
     userInput: null,
   },
   {
-    word: "never",
+    word: "playwright",
     userInput: null,
   },
   {
-    word: "gonna",
+    word: "fuchsia",
     userInput: null,
   },
   {
-    word: "let",
+    word: "minuscule",
     userInput: null,
   },
   {
-    word: "you",
+    word: "ingenious",
     userInput: null,
   },
   {
-    word: "down",
+    word: "sacrilegious",
     userInput: null,
   },
   {
-    word: "never",
+    word: "accommodate",
     userInput: null,
   },
   {
-    word: "gonna",
+    word: "mischievous",
     userInput: null,
   },
   {
-    word: "run",
+    word: "acquiesce",
     userInput: null,
   },
   {
-    word: "around",
+    word: "conscientious",
     userInput: null,
   },
   {
-    word: "and",
+    word: "paraphernalia",
     userInput: null,
   },
   {
-    word: "dessert",
+    word: "onomatopoeia",
     userInput: null,
   },
   {
-    word: "you",
+    word: "vacuum",
     userInput: null,
   },
   {
-    word: "never",
-    userInput: null,
-  },
-  {
-    word: "gonna",
-    userInput: null,
-  },
-  {
-    word: "make",
-    userInput: null,
-  },
-  {
-    word: "you",
-    userInput: null,
-  },
-  {
-    word: "cry",
-    userInput: null,
-  },
-  {
-    word: "never",
-    userInput: null,
-  },
-  {
-    word: "gonna",
-    userInput: null,
-  },
-  {
-    word: "say",
-    userInput: null,
-  },
-  {
-    word: "goodbye",
-    userInput: null,
-  },
-  {
-    word: "never",
-    userInput: null,
-  },
-  {
-    word: "gonna",
-    userInput: null,
-  },
-  {
-    word: "tell",
-    userInput: null,
-  },
-  {
-    word: "a",
-    userInput: null,
-  },
-  {
-    word: "lie",
-    userInput: null,
-  },
-  {
-    word: "and",
-    userInput: null,
-  },
-  {
-    word: "hurt",
-    userInput: null,
-  },
-  {
-    word: "you",
+    word: "irascible",
     userInput: null,
   },
 ];
-
-// export default [
-//     {
-//       word: "nauseous",
-//       userInput: null
-//     },
-//     {
-//       word: "dilate",
-//       userInput: null
-//     },
-//     {
-//       word: "indict",
-//       userInput: null
-//     },
-//     {
-//       word: "liquefy",
-//       userInput: null
-//     },
-//     {
-//       word: "bologna",
-//       userInput: null
-//     },
-//     {
-//       word: "playwright",
-//       userInput: null
-//     },
-//     {
-//       word: "fuchsia",
-//       userInput: null
-//     },
-//     {
-//       word: "minuscule",
-//       userInput: null
-//     },
-//     {
-//       word: "ingenious",
-//       userInput: null
-//     },
-//     {
-//       word: "sacrilegious",
-//       userInput: null
-//     },
-//     {
-//       word: "accommodate",
-//       userInput: null
-//     },
-//     {
-//       word: "mischievous",
-//       userInput: null
-//     },
-//     {
-//       word: "acquiesce",
-//       userInput: null
-//     },
-//     {
-//       word: "conscientious",
-//       userInput: null
-//     },
-//     {
-//       word: "paraphernalia",
-//       userInput: null
-//     },
-//     {
-//       word: "onomatopoeia",
-//       userInput: null
-//     },
-//     {
-//       word: "vacuum",
-//       userInput: null
-//     },
-//     {
-//       word: "irascible",
-//       userInput: null
-//     },
-//   ];
